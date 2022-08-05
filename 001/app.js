c0c824b0a7d576b07928b3779c3a45d10779e46b
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-05 14:11:16 +0300

    Initial

--- 001/app.js
+++ 001/app.js
@@ -25,10 +25,74 @@ function pirma() {
  let max = Math.max(a,b);
  let min = Math.min(a,b);
 
+console.log(max / min);
  
 }
 
 pirma()
+
+/*!--Naudokite funkcija rand(). Sukurkite tris kintamuosius ir naudodamiesi funkcija rand()
+ jiems priskirkite atsitiktines reikšmes nuo 0 iki 25.
+ Raskite ir su console.log() atspausdinkite kintąmąjį turintį vidurinę reikšmę.--*/
+
+ function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function trecia() {
+    let a = rand (0,25);
+    let b = rand (0,25);
+    let c = rand (0,25);
+
+    let max = Math.max(a,b,c);
+    let min = Math.min(a,b,c);
+
+    console.log (b);
+}
+ trecia()
+
+ /*!--Įvedami skaičiai -a, b, c –kraštinių ilgiai, 
+ trys kintamieji (naudokite ​rand()​ funkcija nuo 1 iki 10). Parašykite skriptą,
+kuris nustatytų, ar galima sudaryti trikampį ir atsakymą atspausdintų naudojant console.log(). --*/
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function trikampis() {
+
+    let a = rand(1 , 10);
+    let b = rand(1 , 10);
+    let c = rand(1 , 10);
+
+    let max = Math.max(a,b,c);
+    let min = Math.min(a,b,c);
+
+    if (a||b||c) { trikampis
+
+        console.log(a+b+c);
+
+    }
+
+}
+
+trikampis()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 let ka;
@@ -87,6 +151,19 @@ let rez = 1;
  }
 
  console.log('*' ,rez);
+
+ switch (kas) {
+    case 'S':
+        console.log('Tikrinam S');
+    case 'M':
+        console.log('Tikrinam M');
+    case 'L':
+        console.log('Tikrinam L');
+    case 'XL':
+        console.log('Tikrinam XL');
+}
+
+
 
 
 
