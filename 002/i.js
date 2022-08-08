33293b72610aff73b0491d9c6c75f85b444aa971
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-08 13:17:35 +0300

    Change

--- 002/i.js
+++ 002/i.js
@@ -4,13 +4,37 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const valioo = function() {
+
+    const kitaFun = function() {
+        return 'Cia buvo kitas Fun';
+    }
+
+    //if (rand(0,1)) {
+    //    return 'Valioo!';
+   // }
+      //  return 'visai ne valio';
+    //}
+
+//console.log(valioo());
 
 
-let iskrito;
 
-do {
+    let spalva;
+
+    spalva='red';
 
 
-    iskrito = rand(0,1) ? 'S' : 'H';
-    console.log(iskrito);
+
+    function color() {
+console.log(spalva)
+
+    }
+    spalva='green';
+    spalva='black';
+
+    color();
+    spalva='pink';
+
+    color();
 }
