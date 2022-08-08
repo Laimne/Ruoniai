9f346c4d22432fc01e3fa7aa3cd73e779ae6288c
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-08 14:30:16 +0300

    ciklai

--- 003/app.js
+++ 003/app.js
@@ -1,3 +1,57 @@
 console.log('taip tikrai as esu');
 
 
+
+let rez = 1;
+
+for (let i = 1; i < 6; i++) {
+
+rez = rez * i;
+
+}
+console.log('*', rez);
+ 
+console.log("---------------------------------------");
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+
+/*
+
+<  ---- >=
+>  ---- <=
+== ---- !=
+|| --- &&
+
+*/
+
+
+
+let turiu = 500;
+
+while(turiu <= 1000) {
+
+turiu = turiu + 100;
+
+}
+
+console.log(turiu);
+
+console.log("--------------------------------------");  
+
+
+let iskrito;
+
+do {
+
+    iskrito = rand(0,1) ? 'SKAICIUS' : 'HERBAS';
+    console.log(iskrito)
+
+}while(iskrito != 'HERBAS');
+
+
+console.log(rand(1,5));
