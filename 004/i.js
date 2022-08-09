6e89301a403c864279daf91fabad7fb72e38c63e
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-09 15:27:47 +0300

    change

--- 004/i.js
+++ 004/i.js
@@ -1,28 +1,82 @@
 
+function fun(a ,b) {
+
+let suma = a+b;
+
+return suma
+}
+
+console.log(fun(5, 8));
 
 console.log('-------------------------------');
 
-function fun2 (...arg) {
+function fun2(a , b, c) {
+
+    let suma  = a+b+c;
+
+    return suma;
+}
+// a = u
+// b = 8
+// c = 7
+
+const u = 10;
+const g = 20;
+
+console.log(fun2(u , g , 7));
+
+//parametras yra kintama reiksme "a ir b"//
+//argumentas konkreti reksme atiduodama i ta parametra//
+
+
+console.log('-------------------------------');
+
+function fun4(a = 200 , b = 42) {
+
+    let suma  = a+b;
+
+    return suma;
+}
+// a = k
+// b = 8
+
+const k = 10;
+
+console.log(fun4(100, 100));
+console.log(fun4(k, k));
+
+
+
+
+
+
+console.log('-------------------------------');
+
+function fun6 (...arg) {
 
 console.log(arg);
 }
-//fun2(5, 7);
+fun6(5, 7);
 
-//fun2(5,7,8);
+fun6(5,7,8);
+
+
+
+console.log('-------------------------------');
 
 const daugyba = function() {
     return 5* 201;
 
 }
 
-const daugybaR = a => {
-    return 5 * 201;
+function fun7   (f) {
+    const rez = f();
+    return rez;
 }
 
-const dalybaR = function(a) {
-    return 5 / 201;
-}
+console.log(fun7(daugyba));
 
-console.log('-------------------------------');
+
+
 
 
--- 004/mas.js
+++ 004/mas.js
@@ -1,12 +1,19 @@
 
 
 const mas = [];
-
+//metodas push uzpildo masyva gyventojais//
 mas.push('Petras');
 mas.push('Ona');
 mas.push('Bebras' , 'kengura' ,'raganosis');
+
+//vietoj Onos galime ikelti barsuka, kai irasome 1, nes petras gyvena 0 aukste//
 mas[1]='Barsukas';
-//pakelia i virsu//
+
+//padaro lentele//
+console.table(mas);
+
+
+//pakelia i virsu ,sukuria nulini auksta ir viska pakelia i virsu//
 mas.unshift('Kazkas');
 
 //NEATSPAUSDINA KAS BUVO, BET KAS YRA DABAR//
@@ -15,14 +22,16 @@ console.log([...mas]);
 
 
 
-mas.pop();
+//mas.pop();
 
 
 
-mas.shift();
+//mas.shift();
 
 
 console.log(mas);
+
+
 // suzinome kiek aukstu yra//
 console.log(mas.length);
 
@@ -32,23 +41,42 @@ console.log(mas.length);
 //antras elementas indeksas, pirmas  elementas gyventojas//
 
 
-mas.forEach((gyventojas, aukstas) => console.log(++aukstas,gyventojas));
+//mas.forEach((gyventojas, aukstas) => console.log(++aukstas,gyventojas));//
 
-const mas2 =[];
+//const mas2 =[];//
 
-mas2.push([5, 8, 12]);
+//mas2.push([5, 8, 12]);//
 
-mas2.push([45, 78, 112]);
+//mas2.push([45, 78, 112]);//
 
-mas2.push([50, 80, 120]);
-mas2.push(null);
+//mas2.push([50, 80, 120]);//
+//mas2.push(null);//
 
 
-//console.table(mas2);
+//console.table(mas2);//
 
-//console.log(mas2[1][2]);
+//console.log(mas2[1][2]);//
 
-mas2.forEach(aukstas => aukstas.forEach(skaicius => console.log(skaicius)));
+//mas2.forEach(aukstas => aukstas.forEach(skaicius => console.log(skaicius)));//
+
+
+
+// for(let i = 0; i < mas.lenght; i++){//
+//     console.log('Dabar:', i, mas[1]) ;//
+//  }
+
+
+//const ka = function(vienasGyventojas, aukstas){
+//    console.log(''Dabar Fancy:' , aukstas, vienasGyventojas);
+//}
+
+
+
+
+
+// vienos eilutes kodas, g-gyventojas, i- aukstas , jei kconsoleje pridesime du++ generuos kiekviena gyventoja atskirai//
+mas.forEach((g, i) => console.log(i, g));
+
 
 
 
