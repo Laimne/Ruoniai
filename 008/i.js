7a9791a2bb8b2229cad1ebf0ba45eb8e37024bde
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-17 10:44:33 +0300

    change

--- 008/i.js
+++ 008/i.js
@@ -1,1 +1,79 @@
 console.log('sveki!');
+
+const m = new Map();
+
+
+//m.set('animal' , 'Bebras');
+//m.set('girl' , 'Birute');
+//m.set({labas: 'helo'},{kaTu: 'nieko'});
+
+
+m.set('5' , {p:'pienas', q:5});
+m.set('18' , {p:'kefyras', q:20});
+
+
+if(m.has(18)) {
+m.set(18,{p:'kefyras', q:m.get(18).q + (10)})
+} else {
+    m.set(18,{p:'kefyras', q:10})
+
+}
+
+const ar =[...m];
+
+console.log(ar);
+
+ar.sort((a,b) => a=>[1].q - b[1].q);
+
+const m2 = new Map(ar);
+
+console.log(m2);
+
+
+m.set('18' , 'kefyras');
+
+
+m.delete('pienasgirl');
+m.delete('18');
+
+//console.log('pienas', m.has(5));
+//console.log('kefyras', m.has(18));
+
+console.log(m);
+
+console.log('----------------------------------------------------------');
+
+
+class stikline {
+
+    constructor(turis){
+        this .turis = turis;
+        this . kiekis = 0;
+
+    }
+
+ipilti(kiekis) {
+    this.kiekis = Math.min(this.turis, this.kiekis + kiekis);
+    }
+    ispilti() {
+        const kiekis = this.kiekis;
+        this.kiekis =0;
+        return kiekis;
+    }
+
+    stiklinejeYra() {
+console.log(`Stiklineje ${this.turis}ml yra ${this.kiekis}ml skyscio`);
+    }
+}
+
+const s1 = new stikline(100);
+const s2 = new stikline(150);
+const s3 = new stikline(200);
+
+// Komponentas
+
+// Propsas
+
+// Steitas
+
+
