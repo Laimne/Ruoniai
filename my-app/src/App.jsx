c0753df0868b433f8f9fda4d1be1a4be72ad8d20
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-17 14:36:59 +0300

    change

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>labas</h1>
+       <h1>Labas</h1>
+       <h2>Viso gero!</h2>
       </header>
     </div>
   );
