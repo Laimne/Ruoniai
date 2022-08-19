d5deb2cdc0b466b7fe118edf6253972e5020895c
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-19 16:43:52 +0300

    change

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css';
 
 
-import Bebras from './Components/009/Bebras';
-import Gele from './Components/Gele';
+//import Bebras from './Components/009/Bebras';
+//import Gele from './Components/Gele';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Bebras></Bebras>
-      <Gele></Gele>
+      {/*<Bebras></Bebras>*/}
+      {/*<Gele></Gele>*/}
         <h1>Labas</h1>
-      <Bebras></Bebras>
-        
+      {/*<Bebras></Bebras>*/}
+  
       </header>
     </div>
   );
--- my-app/src/Components/009/Bebras.jsx
+++ my-app/src/Components/009/Bebras.jsx
@@ -1,6 +1,43 @@
-function Bebras() {
+import rand from "../../Functions/rand";
 
-    return<h2>As Bebras</h2>
-}
 
-export default  Bebras;
+//function Bebras() {
+//   return<h2>As Bebras</h2>
+//}
+//export default Bebras;
+
+   function Bebras() {
+
+   const ka = rand(0 , 1);
+
+    return (
+
+  // vietoj div tagu galime naudoti tuscia taga <> ir </>
+  //jeigu norime keliu eiluciu reikia po return deti skliaustus ir juose rasyti
+  // jei norime rasyti koda dedame i garbanotus skliaustus pvz {5 * 8}
+  //jei vietoj nulio irasysime pvz 5 bus Jo, o kai irasai 0 arba fale tai Ne jo
+  //h2>As Bebras sako: {0 ? 'Jo':'Ne jo'} </h2>
+  //ka yra kintamasis kuris generuoja
+  //h2>As Bebras sako: {ka ? 'Jo':'Ne jo'} </h2>
+  //jei yra tagas kabuciu nereikia
+  //h2>As Bebras sako: {ka ? <i>Jo</i>:'Ne jo'} </h2>
+  //kai norim daryti staila turime rasyti
+//<span style={{ color:'crimson'}}>Jo!</span>
+
+    <div>
+
+    <h2>As Bebras sako: {ka ? <i>Jo {8 * 10}</i>:'Ne jo'} </h2>
+    <span style={
+        {
+            color:'crimson',
+            backgraundColor: 'skyblue'
+        }
+            
+            }>Jo!</span>
+      
+
+</div>
+    )
+ }
+
+ export default Bebras;
