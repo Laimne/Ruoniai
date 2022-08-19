724efdbfa629a9e9f6ba7e8566216ac1a41c523d
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-19 17:39:13 +0300

    Black White

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
-
-
+import Black from './Components/Black';
+import White from './Components/White';
+import rand from './Functions/rand';
 //import Bebras from './Components/009/Bebras';
 //import Gele from './Components/Gele';
-
+//rodo arba viena komponenta arba kita parefresinus  { rand(0 , 1) ? <Black/> : <White/>}
 function App() {
   return (
     <div className="App">
@@ -12,7 +13,13 @@ function App() {
       {/*<Gele></Gele>*/}
         <h1>Labas</h1>
       {/*<Bebras></Bebras>*/}
-  
+
+      {
+rand(0 , 1) ? <Black/> : <White/>
+      }
+
+
+
       </header>
     </div>
   );
---
+++ my-app/src/Components/White.jsx
@@ -0,0 +1,8 @@
+function White() {
+
+    return (
+        <div className="white">White</div>
+    )
+}
+
+export default White;
