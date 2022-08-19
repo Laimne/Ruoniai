a0629c3b2f9ff719ae712c4460e4745d76198c59
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-19 14:58:49 +0300

    COMPONENTS

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 
-// import Bebras from './Components/009/Bebras';
+
+import Bebras from './Components/009/Bebras';
+import Gele from './Components/Gele';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Bebras></Bebras> */}
+      <Bebras></Bebras>
+      <Gele></Gele>
         <h1>Labas</h1>
-        {/* <Bebras></Bebras> */}
+      <Bebras></Bebras>
         
       </header>
     </div>
