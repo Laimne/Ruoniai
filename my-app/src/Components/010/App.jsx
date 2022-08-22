44de7fee228a8d459aaa0a2d7072355c174c88d8
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-22 11:10:19 +0300

    change

--- my-app/src/Components/010/App.jsx
+++ my-app/src/Components/010/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
+import Square from '../../Components/010/Square';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
         <h1>Props</h1>
-
-
+        <Square color="black" name="Pirmadienis" size={200}></Square>
+        <Square color="crimson" name="Valio!" size={333}></Square>
       </header>
     </div>
   );
