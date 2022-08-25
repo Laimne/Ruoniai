e8140a591a138a2ccd6ffcaf233d8bf1738c4ae9
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-25 09:40:46 +0300

    Change

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import './App.scss';
-import Dog from './Components/012/Dog';
+
 
 function App() {
+
+const[count,setCount]= useState(3);
+
+const mult5= ()=> {
+    setCount(c=>c*5);
+    //setCount(c=>c*5);
+}
+
+const reset3 =() => {
+    setCount(3);
+}
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>STATE</h1>
-        <Dog color="crimson" />
-      </header>
+        <h1>Caunt{count}</h1>
+<button onClick={mult5}>M1</button>
+<button onClick={reset3}>M2</button>
+
+          </header>
     </div>
   );
 }
